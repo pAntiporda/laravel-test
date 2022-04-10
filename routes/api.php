782074e4e31f5f12e1d600a3e71607de75adb59c
Phil Antiporda <phil.antiporda@growthops.com.au>
2022-04-11 09:13:15 +1000

    feat: init Post model, initial route and intregration

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Welcome to the blog API.',
+    ]);
+});
+
+Route::get('/posts', function () {
+    return Post::all();
+});
+
+Route::get('/posts/{post}', function ($id) {
+    return Post::find($id);
 });
