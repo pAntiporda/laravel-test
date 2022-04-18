849e0c54208d89f9593d337f2acd0222bfebd911
Phil Antiporda <phil.antiporda@growthops.com.au>
2022-04-19 01:12:50 +1000

    feat: update seeder file to seed Post, Category and User data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Category;
+use \App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+
+        User::factory(3)->create();
+        $personal = Category::create(['name' => 'Personal', 'slug' => 'personal']);
+        $work = Category::create(['name' => 'Work', 'slug' => 'work']);
+        Category::create(['name' => 'Family', 'slug' => 'family']);
+        Post::create([
+            'category_id' => $personal->id,
+            'user_id' => User::first()->id,
+            'title' => 'First Post',
+            'slug' => 'first-post',
+            'excerpt' => 'This is the first post',
+            'body' => 'This is the first post'
+        ]);
+        Post::create([
+            'category_id' => $work->id,
+            'user_id' => User::first()->id,
+            'title' => 'Second Post',
+            'slug' => 'second-post',
+            'excerpt' => 'This is the second post',
+            'body' => 'This is the second post'
+        ]);
     }
 }
